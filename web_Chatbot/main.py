1628a09fbaf8bea6d26202f504808b28a87d3b4c
Aman8T <77984568+Aman8T@users.noreply.github.com>
2024-05-09 21:57:47 +0530

    Update main.py

--- web_Chatbot/main.py
+++ web_Chatbot/main.py
@@ -38,7 +38,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 #url: str,question: str
-llm = OpenAI(model="gpt-3.5-turbo",temperature=2,max_tokens=1024)
+llm = ChatOpenAI(model="gpt-3.5-turbo",temperature=2,max_tokens=1024)
 
 @app.post("/predict", response_model=Response)
 def predict(payload: dict) -> Any:
